44488d55f5d27ed83caf659468b01cfd349558de
Ikwunze Kelvin <ikwunzekelvin@gmail.com>
2026-08-21 10:46:27 +0100

    chore: add 404 not found test

--- backend/tests/health_check.rs
+++ backend/tests/health_check.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use backend::startup::Application;
+use reqwest::StatusCode;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -21,6 +22,23 @@ async fn health_check_works() {
         .unwrap();
 
     assert!(response.status().is_success());
+    assert_eq!(response.status(), StatusCode::OK);
 
 
+}
+
+
+#[tokio::test]
+
+async fn non_existing_route_returns_404 () {
+ let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+ let application = Application::build(listener).await.unwrap();
+ let address = format!("http://127.0.0.1:{}", application.port());
+
+ tokio::spawn(application.run_until_stopped());
+ let client = reqwest::Client::new();
+
+ let response = client.get(format!("{}/exit", address)).send().await.unwrap();
+
+ assert_eq!(response.status(), StatusCode::NOT_FOUND);
 }
