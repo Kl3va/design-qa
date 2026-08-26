0554e5eee1e3e1fd7861f38fa5f7348668d1c3f8
Ikwunze Kelvin <ikwunzekelvin@gmail.com>
2026-08-26 15:05:33 +0100

    chore: add test suites for persistence in db for projects

--- backend/tests/helpers.rs
+++ backend/tests/helpers.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 
 use backend::{configuration::get_configuration, startup::Application};
 
+
+
 pub async fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").unwrap();
     let config = get_configuration().unwrap();
---
+++ backend/tests/projects.rs
@@ -0,0 +1,51 @@
+use backend::{configuration::get_configuration, startup::get_connection_pool};
+use reqwest::StatusCode;
+
+use crate::helpers::spawn_app;
+
+mod helpers;
+
+#[tokio::test]
+
+async fn create_project_returns_201_for_valid_name() {
+ let address = spawn_app().await;
+
+ let client = reqwest::Client::new();
+
+ let response = client.post(format!("{}/projects", address)).json(&serde_json::json!({"name": "My project"})).send().await.unwrap();
+
+ assert_eq!(response.status(), StatusCode::CREATED);
+}
+
+#[tokio::test]
+async fn create_project_returns_400_bad_requesst_with_invalid_data() {
+ let address = spawn_app().await;
+
+ let client = reqwest::Client::new();
+
+ let response = client.post(format!("{}/projects", address)).json(&serde_json::json!({"name":""})).send().await.unwrap();
+
+ assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+}
+
+#[tokio::test]
+
+async fn create_project_confirms_data_exists_in_database () {
+ let address = spawn_app().await;
+ let client = reqwest::Client::new();
+
+ let response = client.post(format!("{}/projects", address)).json(&serde_json::json!({"name": "New Project"})).send().await.unwrap();
+
+ assert_eq!(response.status(), StatusCode::CREATED);
+
+ let body:serde_json::Value = response.json().await.unwrap();
+ let project_id = body["project_id"].as_str().unwrap();
+
+ let config = get_configuration().unwrap();
+
+ let pool = get_connection_pool(&config.database);
+
+ let result = sqlx::query!(r#"SELECT name, project_id FROM projects WHERE project_id = $1"#, uuid::Uuid::parse_str(project_id).unwrap()).fetch_one(&pool).await.unwrap();
+
+ assert_eq!(result.name, "New Project");
+}
